821ec61cbaa6be81c7175be93017d513a76964a0
Daniel Lee <lihongyangnju@gmail.com>
2026-01-07 11:40:35 +0800

    Initial Commit

---
+++ Bible for Ladies/Bible_for_LadiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_for_LadiesApp.swift
+//  Bible for Ladies
+//
+//  Created by Mercury on 2026/1/7.
+//
+
+import SwiftUI
+
+@main
+struct Bible_for_LadiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
